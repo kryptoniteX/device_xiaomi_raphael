caaaa29974ed8e5178006bdfd136027f35ed4e85
kryptoniteX <punitrajgautam@gmail.com>
2021-08-31 02:09:41 +0530

    revengeos mk

--- revengeos_raphael.mk
+++ revengeos_raphael.mk
@@ -23,7 +23,10 @@ TARGET_USES_BLUR := true
 #TARGET_INCLUDE_STOCK_ARCORE := true
 $(call inherit-product, vendor/revengeos/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
-
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_MINIMAL_APPS := false
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
 PRODUCT_PRODUCT_PROPERTIES += \
   ro.nezuko.cpu=SD855
 
